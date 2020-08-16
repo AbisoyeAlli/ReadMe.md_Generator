72b9ee3bde7a46e11d949da812e99bf978349c8f
corporate-coder <abisoyeoladayo@gmail.com>
2020-08-16 13:29:33 +0100

    Add files via upload

--- generate.js
+++ generate.js
@@ -1,18 +1,22 @@
+const fs = require('fs');
 const readline = require('readline');
+const path = require('path');
+
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 rl.question('What is your project title? ', function(title){
+    const file = 'ReadMe.md'
     rl.question('What installations are required? ', function(installations){
         rl.question('What are the use cases of your project? ', function(usage){
             rl.question("What are the steps to contributing to your project? ", function(contributing){
-                console.log(`
-                ${title}
-                ${installations}
-                ${usage}
-                ${contributing}`);
+                console.log(`${title}`);
+                fs.writeFileSync(path.join(__dirname,file),"\n"+"# " + title  + "\n")
+						        fs.appendFileSync(file, "\n" + "## Installations"+ "\n" + installations +"\n")
+						        fs.appendFileSync(file, "\n" + "## Use Cases"+ "\n" + usage +"\n")
+						        fs.appendFileSync(file, "\n" + "## Contributing"+ "\n" + contributing+"\n")
                 rl.close();
             });
         });
@@ -23,3 +27,4 @@ rl.question('What is your project title? ', function(title){
         console.log('Thank you!');
         process.exit(0);
     });
+
